90299d0ab7412d81b47aaa38d6fbe86b7be7fc07
Christian Schulze <accounts@andinfinity.de>
2023-08-17 10:31:24 +0200

    feat(protagonist): removed crawling restriction and made cmd configurable

---
+++ protagonist/adapters/crawl/colly.go
@@ -0,0 +1,117 @@
+package crawl
+
+import (
+	"bytes"
+
+	"github.com/ChristianSch/hiro/protagonist/domain/model/crawl"
+	"github.com/ChristianSch/hiro/protagonist/domain/model/index"
+	"github.com/gocolly/colly/v2"
+	"github.com/gocolly/colly/v2/extensions"
+	"go.uber.org/zap"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type CollyConfig struct {
+	// Proxies []string
+	Indexer  index.PageIndexer
+	MaxDepth *int
+}
+
+type CollyCrawler struct {
+	c       *colly.Collector
+	cfg     CollyConfig
+	indexer index.PageIndexer
+}
+
+func NewCollyCrawler(cfg CollyConfig) *CollyCrawler {
+	var maxDepth int = 2
+	if cfg.MaxDepth != nil {
+		maxDepth = *cfg.MaxDepth
+	}
+
+	c := colly.NewCollector(
+		colly.MaxDepth(maxDepth),
+		// rate limit
+		// random user agent
+	)
+
+	extensions.RandomUserAgent(c)
+
+	// load proxies into round robin switcher
+	// rp, err := proxy.RoundRobinProxySwitcher(proxies.GetAll()...) // list of proxy strings
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// if using async then disable transport keep alives
+	// c.WithTransport(&http.Transport{
+	// 	Proxy:             rp,
+	// 	DisableKeepAlives: true, // must be true
+	// })
+
+	return &CollyCrawler{
+		c:       c,
+		cfg:     cfg,
+		indexer: cfg.Indexer,
+	}
+}
+
+func (c *CollyCrawler) Crawl(url string) error {
+	c.c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		e.Request.Visit(e.Attr("href"))
+	})
+
+	var err error = nil
+
+	c.c.OnResponse(func(res *colly.Response) {
+		pageUrl := res.Request.URL.String()
+		zap.L().Info("received response", zap.String("url", pageUrl))
+		// convert body to reader
+		body := bytes.NewReader(res.Body)
+
+		// parse body for links
+		doc, err2 := goquery.NewDocumentFromReader(body)
+		if err != nil {
+			err = err2
+		} else {
+			bodyText := doc.Find("body").Text()
+
+			links := []crawl.CrawlReference{}
+
+			doc.Find("a").Each(func(i int, s *goquery.Selection) {
+				link, _ := s.Attr("href")
+				links = append(links, link)
+
+				res.Request.Visit(link)
+			})
+
+			title := doc.Find("title").Text()
+			description, _ := doc.Find("meta[name=description]").Attr("content")
+
+			crawl := crawl.CrawlResult{
+				Url:         pageUrl,
+				Title:       title,
+				Description: description,
+				Body:        string(bodyText),
+				References:  links,
+			}
+
+			zap.L().Info("crawled page",
+				zap.String("url", crawl.Url),
+				zap.String("title", crawl.Title),
+				zap.String("description", crawl.Description),
+				zap.Int("references", len(crawl.References)))
+
+			c.indexer.PutPage(crawl)
+		}
+	})
+
+	c.c.OnRequest(func(r *colly.Request) {
+		zap.L().Info("Visiting", zap.String("url", r.URL.String()))
+	})
+
+	c.c.Visit(url)
+
+	return err
+}
--- protagonist/cmd/crawl.go
+++ protagonist/cmd/crawl.go
@@ -1,23 +1,51 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/ChristianSch/hiro/protagonist/adapters/crawl"
 	"github.com/ChristianSch/hiro/protagonist/adapters/index"
 	"github.com/ChristianSch/hiro/protagonist/infra/logging"
 	"go.uber.org/zap"
 )
 
+type AppConfig struct {
+	Debug         bool
+	StartingPoint string
+	MaxDepth      int
+}
+
 func main() {
-	logger := logging.InitLogger(true)
+	var maxDepth = flag.Int("max-depth", 2, "max depth of crawling")
+	var startingPoint = flag.String("url", "", "starting point of crawling")
+	var debug = flag.Bool("debug", false, "debug mode")
+
+	flag.Parse()
+
+	if *startingPoint == "" {
+		panic("Url is mandatory. Usage: ./crawl -url https://andinfinity.eu")
+	}
+
+	// get the starting point
+	run(AppConfig{
+		Debug:         *debug,
+		StartingPoint: *startingPoint,
+		MaxDepth:      *maxDepth,
+	})
+}
+
+func run(cfg AppConfig) {
+	logger := logging.InitLogger(cfg.Debug)
 	zap.ReplaceGlobals(logger)
 
 	indexer := index.NewWintermuteIndexer(index.WintermuteConfig{
 		Host: "localhost:50052",
 	})
 	crawler := crawl.NewCollyCrawler(crawl.CollyConfig{
-		Indexer: indexer,
+		Indexer:  indexer,
+		MaxDepth: &cfg.MaxDepth,
 	})
-	err := crawler.Crawl("https://andinfinity.eu")
+	err := crawler.Crawl(cfg.StartingPoint)
 	if err != nil {
 		panic(err)
 	}
